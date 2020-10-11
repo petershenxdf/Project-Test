3190d4ed5d4078151d65a1cbd3eb230274e24e55
qawazswse <31367515+qawazswse@users.noreply.github.com>
2020-10-11 02:11:36 -0500

    加了login和sign up

---
+++ js/customer_login.js
@@ -0,0 +1,9 @@
+function sendForm(){
+            // 先写着，万一能用上呢
+            let result = document.querySelector('.result'); 
+            let username = document.querySelector('#username'); 
+            let password = document.querySelector('#password');
+
+            let url = "index.html";
+            window.location.href=url;
+        } 
