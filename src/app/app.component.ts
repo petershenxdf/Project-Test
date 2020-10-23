9ed4c375f52c823e2eaf7f22fa6fa466170f84e4
cj3699xiao <354123302@qq.com>
2020-10-23 01:11:29 -0500

    setup nav enviroment & nav version1 finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-project';
+  title = 'Group-5 Project';
   dataType: string = "";
   sortMethod: string = "Highest";
   dateOfData: Date = new Date("05/21/2020");
@@ -18,17 +18,17 @@ export class AppComponent {
 
   ngOnInit () {  }
 
-  toggle() {
-    this.show = !this.show;
+  // toggle() {
+  //   this.show = !this.show;
 
-    // CHANGE THE NAME OF THE BUTTON.
-    if(this.show){
-      this.buttonName = "Hide Date Table";
-    } else {
-      this.buttonName = "Show Date Table";
-    }
+  //   // CHANGE THE NAME OF THE BUTTON.
+  //   if(this.show){
+  //     this.buttonName = "Hide Date Table";
+  //   } else {
+  //     this.buttonName = "Show Date Table";
+  //   }
       
-    console.log(this.show);
-  }
+  //   console.log(this.show);
+  // }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChartComponent } from './chart/chart.component';
 import { BarComponent } from './bar/bar.component';
-import { LoginComponent } from './login/login.component';
+// import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChartComponent,
-    BarComponent,
-    LoginComponent
+    BarComponent
+    // LoginComponent
   ],
   imports: [
     BrowserModule,
