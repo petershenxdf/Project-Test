df29e8fd13831b73ce480c266fb0dbabe1c04b27
qawazswse <31367515+qawazswse@users.noreply.github.com>
2020-11-07 19:48:03 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAcQp80G_xyNgf7Ow3MTtvmz1aus0dcDlw',
+    authDomain: 'fir-project1-1118b.firebaseapp.com',
+    databaseURL: 'https://fir-project1-1118b.firebaseio.com',
+    projectId: 'fir-project1-1118b',
+    storageBucket: 'fir-project1-1118b.appspot.com',
+    messagingSenderId: '734288531620',
+    appId: '1:734288531620:web:88973312ff6c37c2df5631',
+    measurementId: 'G-3SJ25R54SR'
+  }
 };
